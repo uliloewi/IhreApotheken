046370c0b16d5efc74aa5e67316de6bbe4f13af7
Li Yu <yu.li@sb-e.de>
2019-01-07 16:39:03 +0100

    new models

---
+++ Models/FunctionToken.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Runtime.Serialization
+<DataContract()>
+Public Class FunctionToken
+    <DataMember>
+    Public Property FunctionofapoID As Integer
+    <DataMember>
+    Public Property token As String
+End Class
---
+++ Models/OrderItem.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Runtime.Serialization
+
+<DataContract>
+Public Class OrderItem
+    <DataMember>
+    Public Property OrderItemID As Integer 'from orderitem
+    <DataMember>
+    Public Property OrderID As String  'from orderitem
+    <DataMember>
+    Public Property Quantity As String  'from orderitem
+    <DataMember>
+    Public Property ProductID As String  'from orderitem
+    <DataMember>
+    Public Property ProductName As String  'from product
+    <DataMember>
+    Public Property OrderDate As String  'from order
+End Class
